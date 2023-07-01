ae10f3980f9be34f8e084acde4e095874020822b
WestHuangwh <1502114880@qq.com>
2023-07-01 21:57:16 +0800

    更改一次

--- Sort.c
+++ Sort.c
@@ -97,6 +97,27 @@ void SelectSort(int* a, int n)
 {
 	assert(a);
 	int begin = 0, end = n - 1;
+	while (begin < end)
+	{
+		int mini = begin, maxi = begin;
+		for (int i = begin + 1; i <= end; i++)
+		{
+			if (a[i] < a[mini])
+			{
+				mini = i;
 
+			}
+			if (a[i] > a[maxi])
+			{
+				maxi = i;
+			}
+
+		}
+		Swap(&a[begin], &a[mini]);
+		Swap(&a[end], &a[maxi]);
+		++begin;
+		--end;
+
+	}
 
 }
--- Test.c
+++ Test.c
@@ -1,13 +1,13 @@
 #include"Sort.h"
 
-void TestInserSort()
+void TestInserSort()//÷±Ω”≤Â»Î≈≈–Ú
 {
 	int a[] = { 2,7,5,3,9,1,10,4,8,6 };
 	InserSort(a, sizeof(a)/sizeof(a[0]));
 	PrintArray(a, sizeof(a) / sizeof(a[0]));
 
 }
-void TestShellSort()
+void TestShellSort()//œ£∂˚≈≈–Ú
 {
 	int a[] = { 2,7,5,3,9,1,10,4,8,6 };
 	ShellSort(a, sizeof(a) / sizeof(a[0]));
